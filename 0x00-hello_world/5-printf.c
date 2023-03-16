fb28cfcfbf19f0dbbad245e203af06742e9dded7
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-03-16 15:05:32 +0100

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Prints ""Programming is like building a multilingual puzzle"
+ * main - Prints "with proper grammar, but the outcome is a piece of art"
  * Return: 0
  */
 int main(void)
