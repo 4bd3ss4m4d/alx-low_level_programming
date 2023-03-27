7735021786b67ead02e3acad40060aeda98c1ea9
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-03-27 17:30:25 +0000

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to the string to compute the length of
+ * Return: the length of the string pointed to by s
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
