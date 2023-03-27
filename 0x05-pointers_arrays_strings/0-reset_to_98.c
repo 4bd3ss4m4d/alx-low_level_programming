5da0f578545378966dfc6de424d008dbd0293ba7
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-03-27 16:59:58 +0000

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of a number it points to to 98
+ * @n: the address of of a number
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
