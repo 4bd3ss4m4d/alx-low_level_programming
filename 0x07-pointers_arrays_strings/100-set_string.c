7df49e2286e07f6dead6660897618db45915bb2d
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-04-04 00:44:53 +0000

    8. Double pointer, double fun

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * set_string - prints the sum of the two diagonals of a square
+ * matrix of integers
+ *
+ * @s: pointer to the pointer to be modified
+ * @to: pointer to a char
+ *
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
