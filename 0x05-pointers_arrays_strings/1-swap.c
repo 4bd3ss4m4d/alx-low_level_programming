d55ac821b84357646a52a7e72c9814b3b89ae97f
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-05 13:49:18 +0100

    Fixing the styling for 1. Don't swap horses in crossing a stream

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,16 +4,14 @@
  * swap_int - swaps the values of two integers
  * @a: the address of the 1st integer
  * @b: the address of the 2nd integer
+ *
  * Return: void
  */
-
 void swap_int(int *a, int *b)
 {
 	int temp;
 
 	temp = *a;
-
 	*a = *b;
 	*b = temp;
 }
-
