f6f8bc5bfaad5b5d72eddd541f2efdd59028464b
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-05 15:33:11 +0100

    Changing syntax for 9. strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,28 +1,25 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _strcpy - copies the string pointed to by src,
- * including the terminating null byte (\0),
- * to the buffer pointed to by dest
+ *	   including the terminating null byte (\0),
+ *	   to the buffer pointed to by dest
  * @dest: pointer to the destination buffer where the string will be copied to
  * @src: pointer to the source string that will be copied
+ *
  * Return: pointer to dest
  */
-
 char *_strcpy(char *dest, char *src)
 {
 
-	int i;
-	int length;
+	int i, len = 0;
 
-	length = strlen(src);
+	while (src[len] != '\0')
+		len++;
 
-	for (i = 0; i <= length; i++)
-	{
+	for (i = 0; i <= len; i++)
 		dest[i] = src[i];
-	}
+	dest[i] = '\0';
 
 	return (dest);
 }
-
