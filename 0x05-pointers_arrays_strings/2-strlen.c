a4268643944544f3f1ba0dbdea67a87b9cb2f89e
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-05 14:00:11 +0100

    Changing syntax for 2. This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,17 @@
-#include <string.h>
 #include "main.h"
 
 /**
  * _strlen - returns the length of a string
  * @s: pointer to the string to compute the length of
+ *
  * Return: the length of the string pointed to by s
  */
-
 int _strlen(char *s)
 {
-	return (strlen(s));
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	return (len);
 }
