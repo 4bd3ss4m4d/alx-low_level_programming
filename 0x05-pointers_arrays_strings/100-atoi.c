ece619d10d9809ab01fe6920a74e72446f04e570
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-06 12:09:14 +0100

    Changing syntax for 10. Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,42 +1,27 @@
-
 #include "main.h"
-#include <stdlib.h>
 
 /**
  * _atoi - convert a string to an integer.
  * @s: string to be converted
+ *
  * Return: an integer
  */
-
 int _atoi(char *s)
 {
-	int sign;
-	unsigned int result;
-	int i;
+	int sign = 1, i;
+	unsigned int result = 0;
 
-	sign = 1;
-	result = 0;
-	i = 0;
-
-	while (s[i])
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == '-')
-		{
 			sign *= -1;
-		}
 
-		if (s[i] >= '0' && s[i] <= '9')
-		{
+		else if (s[i] >= '0' && s[i] <= '9')
 			/* Multiply by 10 to shift the digit to the left */
 			result = result * 10 + (s[i] - '0');
-		}
 
-		if (result != 0 && !(s[i] >= '0' && s[i] <= '9'))
-		{
+		else if (result != 0 && !(s[i] >= '0' && s[i] <= '9'))
 			break;
-		}
-
-		i++;
 	}
 
 	return (result * sign);
