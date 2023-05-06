bcb1f17ffcd92a8e3c0e04e4ca00a6d113365be1
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-06 21:53:10 +0100

    Changing syntax for 0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,39 +2,25 @@
 
 /**
  * _strcat - concatenates two strings
- *
  * @dest: pointer to the destination string
  * @src: pointer to the source string
  *
  * Return: pointer to the resulting string
  */
-
 char *_strcat(char *dest, char *src)
 {
-	int dest_length;
-	int src_length;
-	int i;
+	int dest_len = 0, src_len = 0, i;
 
-	dest_length = 0;
-	src_length = 0;
+	while (dest[dest_len] != '\0')
+		dest_len++;
 
-	while (dest[dest_length] != '\0')
-	{
-		dest_length++;
-	}
+	while (src[src_len] != '\0')
+		src_len++;
 
-	while (src[src_length] != '\0')
-	{
-		src_length++;
-	}
+	for (i = 0; i < src_len; i++)
+		dest[dest_len + i] = src[i];
 
-	for (i = 0; i < src_length; i++)
-	{
-		*(dest + dest_length + i) = *(src + i);
-	}
-
-	*(dest + dest_length + i) = '\0';
+	dest[dest_len + i] = '\0';
 
 	return (dest);
 }
-
