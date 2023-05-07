f04047c2d799bbf6ff7409cb9f414fbbf4b6279a
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-07 18:44:27 +0100

    Changing styling for 8. rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,27 +2,25 @@
 
 /**
  * rot13 - encodes a string using rot13
- *
  * @s: pointer to a string
  *
  * Return: Pointer to encoded string
  */
-
 char *rot13(char *s)
 {
-	char *in, *out;
+	char *mapping_from, *mapping_to;
 	int i, j;
 
-	in = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	out = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	mapping_from = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	mapping_to = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; in[j] != '\0'; j++)
+		for (j = 0; mapping_from[j] != '\0'; j++)
 		{
-			if (s[i] == in[j])
+			if (s[i] == mapping_from[j])
 			{
-				s[i] = out[j];
+				s[i] = mapping_to[j];
 				break;
 			}
 		}
