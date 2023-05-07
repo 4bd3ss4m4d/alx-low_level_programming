2a00cca07f9d034df0288f92fd570cf9a535d409
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-07 18:24:02 +0100

    Changing styling for 7. Mozart composed his music not for the elite, but for everybody

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,28 +2,24 @@
 
 /**
  * leet - encodes a string into 1337
- *
  * @s: pointer to a string
  *
  * Return: Pointer to encoded string
  */
-
 char *leet(char *s)
 {
-	char *leet, *encoding;
+	char *leet, *leet_encoding;
 	int i, j;
 
 	leet = "oOlLeEaAtT";
-	encoding = "0011334477";
+	leet_encoding = "0011334477";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
 			if (s[i] == leet[j])
-			{
-				s[i] = encoding[j];
-			}
+				s[i] = leet_encoding[j];
 		}
 	}
 
