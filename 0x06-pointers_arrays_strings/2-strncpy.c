2a5afc2674cbb0cfc40b29cf81c6abbab61f263b
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-07 10:39:07 +0100

    Changing syntax for 2. strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,27 +2,24 @@
 
 /**
  * _strncpy - copies a string
- *
  * @dest: pointer to the destination string
  * @src: pointer to the source string
  * @n: maximum number of bytes to be copied from src to dest
  *
  * Return: pointer to the resulting string dest
  */
-
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int i, src_len = 0;
+
+	while (src[src_len] != '\0')
+		src_len++;
 
 	for (i = 0; (i < n) && (src[i] != '\0'); i++)
-	{
-		*(dest + i) = *(src + i);
-	}
+		dest[i] = src[i];
 
-	for (; i < n; i++)
-	{
-		*(dest + i) = '\0';
-	}
+	for (i = src_len; i < n; i++)
+		dest[i] = '\0';
 
 	return (dest);
 }
