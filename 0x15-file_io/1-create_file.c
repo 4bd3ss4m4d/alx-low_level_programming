8287924bdf9b3c6d63f378da44ff8bcc6833f020
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-09 00:14:44 +0100

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,12 +1,8 @@
-#include <fcntl.h>
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 
 /**
  * create_file - creates a file with the given name and writes the
- *	         given text content to it.
+ *	       given text content to it.
  * @filename: name of the file to create
  * @text_content: content to write to the file
  *
@@ -14,15 +10,15 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, bytes_written, text_len;
+	int fd, bytes_written, text_len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (text_len = 0; text_content[text_len];)
-			text_len++;
+		for (text_len = 0; text_content[text_len]; text_len++)
+			;
 	}
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
