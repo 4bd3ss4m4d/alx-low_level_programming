9991bdc44a8ffde54217c8aa64e64d0f8a70141c
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-09 16:38:33 +0100

    Changing syntax for 11. It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,37 +1,11 @@
 #include "main.h"
+#include <stdio.h>
 
-/**
- * rev_string - reverses a string
- *
- * @s: pointer to the string to reverse
- *
- * Return: void
- */
-
-void rev_string(char *s)
-{
-	int len;
-	int i, j;
-	char temp;
-
-	len = 0;
-
-	while (s[len])
-	{
-		len++;
-	}
-
-	for (i = 0, j = len - 1; i < j; i++, j--)
-	{
-		temp = s[i];
-		s[i] = s[j];
-		s[j] = temp;
-	}
-}
+int _strlen(char *s);
+void rev_string(char *s);
 
 /**
  * infinite_add - prints an integer
- *
  * @n1: pointer to the 1st number represented as a string
  * @n2: pointer to the 2nd number represented as a string
  * @r: pointer to a character array that will store the result
@@ -39,45 +13,69 @@ void rev_string(char *s)
  *
  * Return: pointer to the result
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int overflow = 0, i = 0, j = 0, digits = 0;
-	int val1 = 0, val2 = 0, temp_tot = 0;
+	int len_n1 = _strlen(n1), len_n2 = _strlen(n2);
+	int i = len_n1 - 1, j = len_n2 - 1, k = 0;
+	int carry = 0, sum = 0;
+	int digit1, digit2;
 
-	while (*(n1 + i) != '\0')
-		i++;
-	while (*(n2 + j) != '\0')
-		j++;
-	i--;
-	j--;
-	if (j >= size_r || i >= size_r)
+	if (len_n1 >= size_r || len_n2 >= size_r)
 		return (0);
-	while (j >= 0 || i >= 0 || overflow == 1)
+
+	while (i >= 0 || j >= 0 || carry == 1)
 	{
-		if (i < 0)
-			val1 = 0;
-		else
-			val1 = *(n1 + i) - '0';
-		if (j < 0)
-			val2 = 0;
-		else
-			val2 = *(n2 + j) - '0';
-		temp_tot = val1 + val2 + overflow;
-		if (temp_tot >= 10)
-			overflow = 1;
-		else
-			overflow = 0;
-		if (digits >= (size_r - 1))
-			return (0);
-		*(r + digits) = (temp_tot % 10) + '0';
-		digits++;
-		j--;
-		i--;
+		digit1 = i >= 0 ? n1[i] - '0' : 0;
+		digit2 = j >= 0 ? n2[j] - '0' : 0;
+		sum = digit1 + digit2 + carry;
+		carry = sum / 10;
+		r[k] = (sum % 10) + '0';
+		i--, j--, k++;
 	}
-	if (digits == size_r)
+	r[k] = '\0';
+
+	if (k > size_r - 1)
 		return (0);
-	*(r + digits) = '\0';
+
 	rev_string(r);
+
 	return (r);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to the string to compute the length of
+ *
+ * Return: the length of the string pointed to by s
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	return (len);
+}
+
+/**
+ * rev_string - reverses a string
+ * @s: pointer to the string to reverse
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i, j = 0;
+	char temp;
+
+	while (s[j] != '\0')
+		j++;
+
+	for (i = 0, j -= 1; i < j; i++, j--)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+	}
 }
