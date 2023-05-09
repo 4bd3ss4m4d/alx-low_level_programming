a47dec941283142fc0ead5978553afde2b52c268
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-09 23:26:56 +0100

    Simplified code logic and optimized resource usage for 2. strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,34 +2,18 @@
 
 /**
  * _strchr - locates a character in a string
- *
  * @s: pointer to the string to search
  * @c: the character to search for
  *
  * Return: pointer to the first occurrence of the character
  */
-
 char *_strchr(char *s, char c)
 {
+	int i;
 
-	unsigned int length;
-	unsigned int i;
-
-	length = 0;
-
-	while (s[length] != '\0')
-	{
-		length++;
-	}
-
-	for (i = 0; i <= length; i++)
-	{
+	for (i = 0; s[i] >= '\0'; i++)
 		if (s[i] == c)
-		{
 			return (&s[i]);
-		}
-	}
 
 	return (0);
 }
-
