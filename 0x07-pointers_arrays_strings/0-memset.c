1c530ee3dcb24fbf8138cc8d8d1d4c1789fc26ac
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-09 22:53:43 +0100

    Changing styling for 0. memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,14 +2,12 @@
 
 /**
  * _memset - fills a memory area with a constant byte
- *
  * @s: pointer to the memory area to be filled
  * @b: the constant byte to be used to fill the memory area
  * @n: the number of bytes to be filled with the constant byte
  *
  * Return: a pointer to the memory area s
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
