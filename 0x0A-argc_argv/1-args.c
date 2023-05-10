e72539a0b037818c3ca7448f1973cd04708e264b
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-10 19:09:27 +0100

    Changing code styling for 1. Silence is argument carried out by other means

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,13 +2,11 @@
 
 /**
  * main - prints the number of arguments passed into it
- *
  * @argc: The number of command-line arguments provided
  * @argv: An array of pointers to the command-line arguments
  *
  * Return: Always 0
  */
-
 int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%i\n", (argc - 1));
