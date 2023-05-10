8999d4895b8a49602038fb5df18bce7f4137f88e
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-10 22:26:52 +0100

    Changing code styling for 4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,16 +1,14 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 /**
  * main - adds positive numbers
- *
  * @argc: The number of command-line arguments provided
  * @argv: An array of pointers to the command-line arguments
  *
  * Return: Always 0
  */
-
 int main(int argc, char *argv[])
 {
 	int i, j, sum = 0;
