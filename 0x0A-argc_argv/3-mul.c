dd73ffeaa685133b0568c2a0cc323afd970ac9a8
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-10 19:38:07 +0100

    Changing code styling for 3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,13 +3,11 @@
 
 /**
  * main - multiplies two numbers
- *
  * @argc: The number of command-line arguments provided
  * @argv: An array of pointers to the command-line arguments
  *
  * Return: Always 0
  */
-
 int main(int argc, char *argv[])
 {
 	if (argc != 3)
