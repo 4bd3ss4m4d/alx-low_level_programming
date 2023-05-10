d0cc5845a5041adb7882b1443c50c36a349c02e1
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-10 19:02:19 +0100

    Changing code styling for 0. It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - prints the name of the program
- *
  * @argc: The number of command-line arguments provided
  * @argv: An array of pointers to the command-line arguments
  *
  * Return: Always 0
  */
-
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
