abcc239b5704230898bc85aa3c41efb49483a6fc
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-26 05:35:54 +0100

    Changing styling for 1. The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
- * main - Compares the last digit of a random number with some values
- * Return: 0
+ * main - Entry point of the program
+ *
+ * Return: Always returns 0 to indicate successful execution
  */
 int main(void)
 {
@@ -11,18 +13,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	if ((n % 10) > 5)
-	{
 		printf("Last digit of %i is %i and is greater than 5\n", n, n % 10);
-	}
 	else if ((n % 10) == 0)
-	{
 		printf("Last digit of %i is %i and is 0\n", n, n % 10);
-	}
 	else if ((n % 10) < 6 && (n % 10) != 0)
-	{
 		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, n % 10);
-	}
 	return (0);
 }
