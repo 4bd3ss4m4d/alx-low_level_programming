6692887cba88cebebc3fca2210f99491d13b88b2
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-26 03:15:36 +0100

    Changing styling for 8. Double pointer, double fun

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,15 +1,13 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * set_string - prints the sum of the two diagonals of a square
- * matrix of integers
- *
+ * set_string - sets the value of a pointer to a char
  * @s: pointer to the pointer to be modified
  * @to: pointer to a char
  *
  * Return: void
  */
-
 void set_string(char **s, char *to)
 {
 	*s = to;
