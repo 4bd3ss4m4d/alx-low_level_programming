dfcfeaea5a1368a25b2b03214d6ae39ae80b4f76
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-26 10:49:34 +0100

    Changing function description for 8. I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,10 @@
 #include "main.h"
+
 /**
- * jack_bauer - Prints every minute of the day of Jack Bauer,
- * starting from 00:00 to 23:59.
- * Return: 0 on Success
+ * jack_bauer - Prints every minute of the day of Jack Bauer
+ *	      starting from 00:00 to 23:59.
+ *
+ * Return: void
  */
 void jack_bauer(void)
 {
@@ -12,12 +14,12 @@ void jack_bauer(void)
 	{
 		for (m = 0; m < 60; m++)
 		{
-			_putchar(h / 10 + '0');
-			_putchar(h % 10 + '0');
-			_putchar(':');
-			_putchar(m / 10 + '0');
-			_putchar(m % 10 + '0');
-			_putchar('\n');
+		_putchar(h / 10 + '0');
+		_putchar(h % 10 + '0');
+		_putchar(':');
+		_putchar(m / 10 + '0');
+		_putchar(m % 10 + '0');
+		_putchar('\n');
 		}
 	}
 }
