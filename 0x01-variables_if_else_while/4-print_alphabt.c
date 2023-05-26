49f1aee410302af1fcf877a57590487bc17ca698
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-26 08:09:21 +0100

    Changing function description for 4. When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Entry point of the program
+ *
+ * Description: This program prints the alphabet in lowercase, excluding the
+ *	        letters 'q' and 'e', followed by a new line.
  *
  * Return: Always returns 0 to indicate successful execution
  */
