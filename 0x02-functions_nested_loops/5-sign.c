2d99d092fb638ebdcbc5d5c5b189373128030182
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-26 10:21:32 +0100

    Changing function description for 5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
- * print_sign - Prints the sign of a number
- * @n: The number to be checked
- * Return: 1 if n is greater than zero, 0 if n is zero.
- * -1 and prints - if n is less than zero
+ * print_sign - Prints the sign of a number.
+ * @n: The number to check the sign
+ *
+ * Return: 1 if n is greater than zero, 0 if n is zero,
+ *	  -1 if n is less than zero
  */
 int print_sign(int n)
 {
