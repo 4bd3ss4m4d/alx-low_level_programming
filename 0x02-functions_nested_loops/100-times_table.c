b932dd77e00a425342a32388b23f934cb5dc4c6d
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-26 12:23:35 +0100

    Changing function description and syntax for 12. The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,44 +1,48 @@
 #include "main.h"
+
 /**
- * print_times_table - prints the n times table, starting with 0
- * @n: Number of the time table
- * Return: Void
+ * print_times_table - Prints the n times table, starting with 0.
+ * @n: The number to generate the times table for.
+ *
+ * Return: void
  */
 void print_times_table(int n)
 {
 	int i, j, k;
 
-	if (n >= 0 && n <= 15)
+	if (n >= 0 && n <= 14)
 	{
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
-				k = j * i;
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				} else if (k < 10 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				} else if (k >= 10 && k < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				} else if (k >= 100)
+				k = i * j;
+				if (k > 99)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar((k / 100) + '0');
 					_putchar(((k / 10) % 10) + '0');
 					_putchar((k % 10) + '0');
+				}
+				else if (k > 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+				else
+				{
+					if (j != 0)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+					}
+				_putchar(k + '0');
 				}
 			}
 			_putchar('\n');
