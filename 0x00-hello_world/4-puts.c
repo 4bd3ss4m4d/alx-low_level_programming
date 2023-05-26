1493993aa9b2da09a46ff4b2afb05287d955008b
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-26 08:40:08 +0100

    Changing function description for 4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point of the program
+ *
+ * Description: This program prints the string "Programming is like building a
+ *	        multilingual puzzle" followed by a new line.
  *
  * Return: Always returns 0 to indicate successful execution
  */
