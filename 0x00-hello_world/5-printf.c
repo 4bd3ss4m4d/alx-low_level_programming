4da57137177e07f5343f21d8b4f985318399e6ac
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-26 08:46:20 +0100

    Changing function description for 5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,6 +3,9 @@
 /**
  * main - Entry point of the program
  *
+ * Description: This program prints the string "with proper grammar, but the
+ *	        outcome is a piece of art," followed by a new line.
+ *
  * Return: Always returns 0 to indicate successful execution
  */
 int main(void)
