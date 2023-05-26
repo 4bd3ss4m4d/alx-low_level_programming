16e09f82c1fbc9ad567d5e9047e040c4d4a09ebf
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-26 07:45:00 +0100

    Changing function description for 1. The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,6 +5,15 @@
 /**
  * main - Entry point of the program
  *
+ * Description: This program generates a random number and prints its last
+ *	        digit. The output of the program includes the string
+ *	        "Last digit of", followed by the value of n, followed by
+ *	        the string "is", and finally the classification of the last
+ *	        digit based on its value. The classification can be "and is
+ *	        greater than 5" if the last digit is greater than 5, "and
+ *	        is 0" if the last digit is 0, or "and is less than 6 and
+ *	        not 0" if the last digit is less than 6 and not equal to 0.
+ *
  * Return: Always returns 0 to indicate successful execution
  */
 int main(void)
@@ -16,7 +25,7 @@ int main(void)
 	if ((n % 10) > 5)
 		printf("Last digit of %i is %i and is greater than 5\n", n, n % 10);
 	else if ((n % 10) == 0)
-		printf("Last digit of %i is %i and is 0\n", n, n % 10);
+	printf("Last digit of %i is %i and is 0\n", n, n % 10);
 	else if ((n % 10) < 6 && (n % 10) != 0)
 		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, n % 10);
 	return (0);
