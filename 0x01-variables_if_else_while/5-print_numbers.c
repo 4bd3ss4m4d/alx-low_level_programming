8912a783665ae4b9860778fefbde1750a71404a0
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-26 08:12:14 +0100

    Changing function description for 5. Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,6 +3,9 @@
 /**
  * main - Entry point of the program
  *
+ * Description: This program prints all single-digit numbers of base 10,
+ *	        starting from 0, followed by a new line.
+ *
  * Return: Always returns 0 to indicate successful execution
  */
 int main(void)
