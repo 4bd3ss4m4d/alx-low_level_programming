52c4a99c412bd43a0e43fdeb30664da1c3c2ccc8
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-26 06:49:13 +0100

    Changing styling and syntax for 9. Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
+
 /**
- * main - Prints all possible combinations of single-digit numbers
- * Return: 0
+ * main - Entry point of the program
+ *
+ * Description: Prints all possible combinations of single-digit numbers in
+ *              ascending order, separated by commas and spaces.
+ *
+ * Return: Always returns 0 to indicate successful execution
  */
 int main(void)
 {
-	int digit;
+	char x;
 
-	/* ASCII code of 0 is 48 */
-	for (digit = 48; digit <= 57; digit++)
-	{
-		putchar(digit);
-		if (digit != 57)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
-
+	for (x = '0'; x <= '9'; x++)
+		putchar(x);
+	for (x = 'a'; x <= 'f'; x++)
+		putchar(x);
 	putchar('\n');
+
 	return (0);
 }
