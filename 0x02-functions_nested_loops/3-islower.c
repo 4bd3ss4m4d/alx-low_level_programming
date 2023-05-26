6a093a761f58c73c366761ea659fc19c6fc26ff2
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-26 10:10:00 +0100

    Changing function description for 3. islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,12 @@
 #include "main.h"
+
 /**
- * _islower - Checks for lowercase character
+ * _islower - Checks if a character is lowercase.
  * @c: The character to be checked
- * Return: 1 on Success and 0 otherwise
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
  */
 int _islower(int c)
 {
-	/* ASCII code of a through z is from 97 to 122 */
-	return (c >= 97 && c <= 122);
+	return (c >= 'a' && c <= 'z');
 }
