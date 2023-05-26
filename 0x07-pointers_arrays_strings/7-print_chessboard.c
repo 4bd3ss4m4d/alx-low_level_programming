212646c9fb079227dbf095cbd58b2aac9ac4f344
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-26 02:35:10 +0100

    Changing styling for 6. Chess is mental torture

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,12 +2,10 @@
 
 /**
  * print_chessboard - prints the chessboard
- *
  * @a: pointer to the 2D array representing the chessboard.
  *
  * Return: void
  */
-
 void print_chessboard(char (*a)[8])
 {
 	int row, col;
@@ -15,11 +13,8 @@ void print_chessboard(char (*a)[8])
 	for (row = 0; row < 8; row++)
 	{
 		for (col = 0; col < 8; col++)
-		{
 			_putchar(a[row][col]);
-		}
 
 		_putchar('\n');
 	}
 }
-
