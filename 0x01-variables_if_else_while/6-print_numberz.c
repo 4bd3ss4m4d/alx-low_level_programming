e564476f65b0e34493bab1075566f952110a984d
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-26 08:15:26 +0100

    Changing function description for 6. Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,6 +3,10 @@
 /**
  * main - Entry point of the program
  *
+ * Description: This program prints all single-digit numbers of base 10,
+ *	        starting from 0, followed by a new line.
+ *	        It accomplishes this without using any variable of type char.
+ *
  * Return: Always returns 0 to indicate successful execution
  */
 int main(void)
