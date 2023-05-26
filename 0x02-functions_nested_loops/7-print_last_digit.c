c626272fada770858cea9bd99257741e61d26c7a
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-26 10:39:54 +0100

    Changing syntax and function description for 7. There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,20 +1,18 @@
 #include "main.h"
+
 /**
- * print_last_digit - Prints the last digit of a number
- * @n: The number to extract the last digit from
- * Return: The last digit
+ * print_last_digit - Prints the last digit of a number.
+ * @n: The number to extract the last digit from.
+ *
+ * Return: The value of the last digit.
  */
 int print_last_digit(int n)
 {
-	int last_digit;
+	int last_digit = n % 10;
 
-	last_digit = n % 10;
-	if (last_digit < 0)
-	{
-		last_digit = -1 * last_digit;
-	}
-
+	if (n < 0)
+		last_digit *= -1;
 	_putchar('0' + last_digit);
-	return (last_digit);
 
+	return (last_digit);
 }
