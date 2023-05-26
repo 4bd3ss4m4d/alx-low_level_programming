8c2e73d58b8470949f5ecec98e2e46bdc102c69c
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-26 08:57:04 +0100

    Changing function description for 8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,16 @@
 /**
  * main - Entry point of the program
  *
- * Return: Always returns 1 to indicate execution
+ * Description: This program prints the string "and that piece of art is
+ *	      useful" - Dora Korpar, 2015-10-19," to the standard error
+ *	      stream.
+ *
+ * Return: Always returns 1 to indicate an error
  */
 int main(void)
 {
 	write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
