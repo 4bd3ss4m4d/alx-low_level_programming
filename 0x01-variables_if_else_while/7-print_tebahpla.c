5554cdcfba4bc1e616017841f3eea9271dc8a278
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-26 08:18:42 +0100

    Changing function description for 7. Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,6 +3,9 @@
 /**
  * main - Entry point of the program
  *
+ * Description: This program prints the lowercase alphabet in reverse, followed
+ *	        by a new line.
+ *
  * Return: Always returns 0 to indicate successful execution
  */
 int main(void)
