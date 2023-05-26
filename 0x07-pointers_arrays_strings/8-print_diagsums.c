bb84a93c2484f4368916d1c548d5ce45d1982c1c
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-26 03:08:25 +0100

    Changing styling and syntax for 7. The line of life is a ragged diagonal between duty and desire

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,24 +3,21 @@
 
 /**
  * print_diagsums - prints the sum of the two diagonals of a square
- * matrix of integers
- *
+ *		    matrix of integers
  * @a: pointer to the first element of the matrix
  * @size: size of the matrix.
  *
  * Return: void
  */
-
 void print_diagsums(int *a, int size)
 {
-	int i, sum1 = 0, sum2 = 0;
+	int i, main_diag_sum = 0, anti_diag_sum = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		sum1 += *(a + i * size + i);
-		sum2 += *(a + i * size + (size - i - 1));
+		main_diag_sum += a[i * size + i];
+		anti_diag_sum += a[i * size + (size - i - 1)];
 	}
 
-	printf("%d, %d\n", sum1, sum2);
+	printf("%d, %d\n", main_diag_sum, anti_diag_sum);
 }
-
