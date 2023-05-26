35b0b1798ca52377aae2794e50f8c96f765ad4be
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-26 10:13:55 +0100

    Changing function description and syntax for 4. isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "main.h"
+
 /**
- * _isalpha - Checks for alphabetic character
+ * _isalpha - Checks if a character is alphabetic.
  * @c: The character to be checked
- * Return: 1 on Success and 0 otherwise
+ *
+ * Return: 1 if c is a letter, 0 otherwise
  */
 int _isalpha(int c)
 {
-	/* ASCII code of a through z is from 97 to 122 */
-	/* ASCII code of A through Z is 65 to 90 */
-	return ((c >= 65 && c <= 90) || (c >= 97 && c <= 122));
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
