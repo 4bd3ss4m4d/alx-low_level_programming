8f1066649877be4dd5b4bccca4ae7d1d202cf888
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-26 07:25:07 +0100

    Changing styling and syntax for 11. The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,22 +1,35 @@
 #include <stdio.h>
+
 /**
- * main - Prints all possible different combinations of three digits
- * Return: 0
+ * main - Entry point of the program
+ *
+ * Description: This program generates and prints all possible combinations
+ *	        of three digits satisfying the following conditions:
+ *	        - The digits must be different.
+ *	        - The smallest combination of three digits is printed.
+ *	        - Numbers are printed in ascending order with three digits.
+ *	        - Each number is separated by a comma and a space.
+ *	        - The putchar function is used for output, and no other
+ *		  output functions are allowed.
+ *	        - The code uses a maximum of six putchar function calls.
+ *	        - No variables of type char are used.
+ *
+ * Return: Always returns 0 to indicate successful execution
  */
 int main(void)
 {
-	int first_digit, second_digit, third_digit;
+	int d1, d2, d3;
 
-	for (first_digit = 48; first_digit < 58; first_digit++)
+	for (d1 = '0'; d1 <= '9'; d1++)
 	{
-		for (second_digit = first_digit + 1; second_digit < 58; second_digit++)
+		for (d2 = d1 + 1; d2 <= '9'; d2++)
 		{
-			for (third_digit = second_digit + 1; third_digit < 58; third_digit++)
+			for (d3 = d2 + 1; d3 <= '9'; d3++)
 			{
-				putchar(first_digit);
-				putchar(second_digit);
-				putchar(third_digit);
-				if (first_digit < 55)
+				putchar(d1);
+				putchar(d2);
+				putchar(d3);
+				if (d1 < '7')
 				{
 					putchar(',');
 					putchar(' ');
@@ -24,7 +37,7 @@ int main(void)
 			}
 		}
 	}
-
 	putchar('\n');
+
 	return (0);
 }
