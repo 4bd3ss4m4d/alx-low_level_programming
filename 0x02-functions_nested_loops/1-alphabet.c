3d7a804a6ecb3b92d73b5a14085a1c70f0f3dee7
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-26 09:49:51 +0100

    Changing function description and syntax for 1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,15 @@
 #include "main.h"
+
 /**
  * print_alphabet - Prints the alphabet, in lowercase
- * Return: Always 0
+ *
+ * Return: void
  */
 void print_alphabet(void)
 {
-	int l;
+	int c;
 
-	l = 'a';
-
-	while (l <= 'z')
-	{
-		_putchar(l);
-		l++;
-	}
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
 	_putchar('\n');
 }
