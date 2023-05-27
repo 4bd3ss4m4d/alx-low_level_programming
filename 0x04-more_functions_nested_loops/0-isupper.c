2b7f84279c49a509eebc6d6aa90cc27ef9ac03fe
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-27 04:34:20 +0100

    Changing function description for 0. isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * _isupper - checks for uppercase character
- * @c: character to be checked
- * Return: 1 if the character is uppercase. 0 otherwise
+ * _isupper - checks if a character is uppercase
+ * @c: the character to be checked
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
  */
-
 int _isupper(int c)
 {
 	return (c >= 'A' && c <= 'Z');
 }
-
