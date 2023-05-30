8d73bfddb0a7b133391e2c9f97b0f5431e5471b9
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-30 07:53:46 +0100

    Changing function description for 3. The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,15 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers, from 0 to 9
+ * print_numbers - prints the numbers from 0 to 9 followed by a new line
+ *
  * Return: void
  */
-
 void print_numbers(void)
 {
-	int c;
+	int i;
 
-	for (c = 0; c < 10; c++)
-	{
-		_putchar(c + '0');
-	}
-
+	for (i = 0; i < 10; i++)
+		_putchar(i + '0');
 	_putchar('\n');
 }
-
