50b696b687e3831c04b9d170b50997ae533cead1
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-30 08:12:27 +0100

    Changing function description and syntax for 4. I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints the numbers, from 0 to 9 except 2 and 4
+ * print_most_numbers - prints the numbers from 0 to 9 (excluding 2 and 4)
+ *                      followed by a new line
+ *
  * Return: void
  */
-
 void print_most_numbers(void)
 {
-	int i;
+	char c;
 
-	for (i = 0; i < 10; i++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		if (i != 2 && i != 4)
-		{
-			_putchar(i + '0');
-		}
+		if (!(c == '2' || c == '4'))
+			_putchar(c);
 	}
-
 	_putchar('\n');
 }
+
