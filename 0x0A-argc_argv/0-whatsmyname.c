29eb3959695e56c3f5f4c307d7b182bc9dca888f
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-30 17:49:51 +0100

    Changing function description for 0. It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,14 +2,19 @@
 #include <stdlib.h>
 
 /**
- * main - prints the name of the program
+ * main - Entry point of the program
  * @argc: The number of command-line arguments provided
  * @argv: An array of pointers to the command-line arguments
+ *
+ * Description: The program retrieves its own name from the command line
+ *	        arguments and prints it, including the path before the name if
+ *	        provided.
  *
  * Return: Always 0
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
+
 	return (0);
 }
