8e4028932563328ed6d7bd06cde38f9051558ab3
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-30 18:17:43 +0100

    Changing function description for 5. Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,12 +2,16 @@
 #include <stdlib.h>
 
 /**
- * main - prints the minimum number of coins
- *	  to make change for an amount of money
- * @argc: The number of command-line arguments provided
- * @argv: An array of pointers to the command-line arguments
+ * main - Entry point of the program
+ * @argc: The number of command line arguments including the program name
+ * @argv: An array of strings containing the command line arguments
  *
- * Return: Always 0
+ * Description: This program calculates the minimum number of coins needed to
+ *	        make change for an amount of money given in cents. The amount
+ *	        of cents is provided as a command line argument. It prints the
+ *	        minimum number of coins required, followed by a new line.
+ *
+ * Return: 0 if calculation is successful, 1 otherwise
  */
 int main(int argc, char *argv[])
 {
@@ -38,5 +42,6 @@ int main(int argc, char *argv[])
 	}
 
 	printf("%d\n", count);
+
 	return (0);
 }
