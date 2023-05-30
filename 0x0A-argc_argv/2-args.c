b672ec45bd5283ef3984bb867dfc916e9ef39eb6
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-30 17:59:26 +0100

    Changing function description 2. The best argument against democracy is a five-minute conversation with the average voter

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - prints all arguments it receives
- * @argc: The number of command-line arguments provided
- * @argv: An array of pointers to the command-line arguments
+ * main - Entry point of the program
+ * @argc: The number of command line arguments including the program name
+ * @argv: An array of strings containing the command line arguments
  *
- * Return: Always 0
+ * Description: This program prints all the arguments it receives.
+ *
+ * Return: Always returns 0 to indicate successful execution
  */
 int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
+	{
 		printf("%s\n", argv[i]);
+	}
 
 	return (0);
 }
