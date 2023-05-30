b769b1feeadc209054a383ed534bfa8fdb37f78c
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-30 13:22:37 +0100

    Change function description and syntax for 8. You are so much sunshine in every square inch

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,27 +1,28 @@
 #include "main.h"
 
 /**
- * print_square - prints a square
- * @size: size of the square
+ * print_square - Prints a square of a given size using the '#' character.
+ * @size: The size of the square.
+ *
  * Return: void
  */
-
 void print_square(int size)
 {
-	int i, j;
-
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size; j++)
-		{
-			_putchar('#');
-		}
-		_putchar('\n');
-	}
-
 	if (size <= 0)
 	{
 		_putchar('\n');
 	}
-}
+	else
+	{
+		int r, c;
 
+		for (r = 0; r < size; r++)
+		{
+			for (c = 0; c < size; c++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
