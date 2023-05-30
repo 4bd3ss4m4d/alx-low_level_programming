6aa3eb0a30a6eda609d996fc2a7ad6a725e17989
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-30 13:03:13 +0100

    Changing function description and syntax for 7. I feel like I am diagonally parked in a parallel universe

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_diagonal - draws a diagonal line on the terminal
- * @n: number of times the character \ should be printed
- * Return: void
+ * print_diagonal - Draws a diagonal line on the terminal.
+ * @n: The number of times the character '\' should be printed.
+ *
+ * Return: void.
  */
-
 void print_diagonal(int n)
 {
 	int i, j;
@@ -25,4 +25,3 @@ void print_diagonal(int n)
 		_putchar('\n');
 	}
 }
-
