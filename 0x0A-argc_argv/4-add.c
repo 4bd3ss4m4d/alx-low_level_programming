beb4273814d3db60e2fd8c4b9667b5f4e4122683
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-05-30 18:09:19 +0100

    Changing function description 4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,11 +3,19 @@
 #include <stdlib.h>
 
 /**
- * main - adds positive numbers
- * @argc: The number of command-line arguments provided
- * @argv: An array of pointers to the command-line arguments
+ * main - Entry point of the program
+ * @argc: The number of command line arguments including the program name
+ * @argv: An array of strings containing the command line arguments
  *
- * Return: Always 0
+ * Description: This program adds positive numbers provided as command line
+ *	        arguments.
+ *	        It prints the result of the addition followed by a new line.
+ *	        If no number is passed to the program, it prints 0 followed
+ *	        by a new line.
+ *	        If one of the numbers contains symbols that are not digits,
+ *	        it prints "Error" followed by a new line and returns 1.
+ *
+ * Return: 0 if addition is successful, 1 otherwise
  */
 int main(int argc, char *argv[])
 {
