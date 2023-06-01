684b70b2373aa74af0cbb981ec9a3bfca5d0e7d5
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-06-01 21:32:29 +0100

    Changing function description for 5. Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,13 +3,12 @@
 int find_sqrt(int n, int guess);
 
 /**
- * _sqrt_recursion - returns the natural square root of a number
+ * _sqrt_recursion - Calculates the natural square root of a number
+ * @n: The number for which the square root is to be calculated
  *
- * @n: number to compute the square root of
- *
- * Return: square root of n,
+ * Return: The natural square root of n,
+ *	   or -1 if n does not have a natural square root.
  */
-
 int _sqrt_recursion(int n)
 {
 	return (find_sqrt(n, 1));
@@ -17,13 +16,11 @@ int _sqrt_recursion(int n)
 
 /**
  * find_sqrt - finds the natural square root of a number
- *
  * @n: the number to find the square root of
  * @guess: the current guess
  *
  * Return: the square root of n
  */
-
 int find_sqrt(int n, int guess)
 {
 	if (n < 0 || guess * guess > n)
