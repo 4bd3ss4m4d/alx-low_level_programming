eb1b4308fcf1d9f32e0097a993618903ff3640db
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-06-03 22:48:30 +0100

    Changing function description for 0. Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,36 +2,34 @@
 #include <stdlib.h>
 
 /**
- * create_array - creates an array of chars, and initializes it
- * with a specific char
- *
- * @size: the size of the array to be created
+ * create_array - creates an array of chars and initializes it with a specific
+ *		char
+ * @size: the size of the array
  * @c: the char to initialize the array with
  *
  * Return: a pointer to the array, or NULL if it fails
  */
-
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char *str_arr;
+	char *str_ptr;
 
 	if (size == 0)
 	{
 		return (NULL);
 	}
 
-	str_arr = malloc(sizeof(char) * size);
+	str_ptr = (char *)malloc(sizeof(char) * size);
 
-	if (str_arr == NULL)
+	if (str_ptr == NULL)
 	{
 		return (NULL);
 	}
 
 	for (i = 0; i < size; i++)
 	{
-		str_arr[i] = c;
+		str_ptr[i] = c;
 	}
 
-	return (str_arr);
+	return (str_ptr);
 }
