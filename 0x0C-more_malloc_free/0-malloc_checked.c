7a3491a8185a6096fa728f5114899193a29e473e
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-06-10 01:34:13 +0100

    Changing function description and syntax for Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 
 /**
- * malloc_checked - allocates memory using malloc
+ * malloc_checked - Allocates memory using malloc.
+ * @b: Size of memory to allocate.
  *
- * @b: the number of bytes to allocate
- *
- * Return: pointer to the allocated memory
+ * Return: Pointer to the allocated memory.
+ *	   If malloc fails, the function causes normal process termination
+ *	   with a status value of 98.
  */
-
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
-
-	ptr = malloc(b);
+	void *ptr = malloc(b);
 
 	if (ptr == NULL)
+	{
 		exit(98);
+	}
 
 	return (ptr);
 }
