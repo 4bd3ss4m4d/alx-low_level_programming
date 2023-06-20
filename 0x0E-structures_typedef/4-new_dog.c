67a853d96087e4b2ff91a066f9d7279986f01561
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-06-20 09:13:25 +0100

    Changing Syntax and function description for 4. A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 char *_strdup(char *str);
@@ -10,24 +11,22 @@ char *_strdup(char *str);
  * @owner: pointer to the owner string
  *
  * Return: pointer to the new dog structure if successful,
- * NULL if memory allocation fails
+ *         NULL if memory allocation fails
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
-	char *name_copy, *owner_copy;
+	dog_t *new_dog = malloc(sizeof(dog_t));
+	char *name_copy = _strdup(name), *owner_copy = _strdup(owner);
 
-	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
+	{
 		return (NULL);
-
-	name_copy = _strdup(name),
-	owner_copy = _strdup(owner);
-
+	}
 	if (name_copy == NULL || owner_copy == NULL)
 	{
 		free(new_dog);
 		free(name_copy);
+		free(owner_copy);
 		return (NULL);
 	}
 
@@ -39,29 +38,37 @@ dog_t *new_dog(char *name, float age, char *owner)
 }
 
 /**
- * _strdup - creates a duplicate of a string in a new memory block
- * @str: pointer to the string to be duplicated
+ * _strdup - copies string given as a parameter.
+ * @str: pointer to the input string
  *
- * Return: a pointer to the duplicated string, or NULL if it fails
+ * Return: pointer to the duplicated string, or NULL if str is NULL or
+ *	   if insufficient memory was available.
  */
 char *_strdup(char *str)
 {
 	char *dup;
-	unsigned int size, i;
+	unsigned int size = 0, i;
 
 	if (str == NULL)
+	{
 		return (NULL);
+	}
 
-	for (size = 0; str[size] != '\0'; size++)
-		;
+	while (str[size] != '\0')
+	{
+		size++;
+	}
 
 	dup = malloc(sizeof(char) * (size + 1));
-
 	if (dup == NULL)
+	{
 		return (NULL);
+	}
 
 	for (i = 0; i <= size; i++)
+	{
 		dup[i] = str[i];
+	}
 
 	return (dup);
 }
