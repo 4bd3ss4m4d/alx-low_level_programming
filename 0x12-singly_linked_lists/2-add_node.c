9f12c15a620be168a40534a23225ec0a8a5ca264
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-06-23 17:19:11 +0100

    Changing function syntax and description for 2. Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,30 +1,35 @@
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
+#include <string.h>
+#include <stdlib.h>
 
 /**
- * add_node - adds a new node at the beginning of a linked list
- * @head: double pointer to the list_t list
- * @str: new string to add in the node
+ * add_node - Adds a new node at the beginning of a list_t list.
+ * @head: Pointer to a pointer to the head of the linked list.
+ * @str: String to be duplicated and added as the data of the new node.
  *
- * Return: the address of the new element, or NULL if it fails
+ * Return: The address of the new element, or NULL if it failed.
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new;
-	unsigned int len = 0;
+	list_t *new_node;
+	size_t str_len = 0;
 
-	while (str[len])
-		len++;
-
-	new = malloc(sizeof(list_t));
-	if (!new)
+	new_node = (list_t *)malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
 		return (NULL);
+	}
 
-	new->str = strdup(str);
-	new->len = len;
-	new->next = (*head);
-	(*head) = new;
+	while (str[str_len] != '\0')
+	{
+		str_len++;
+	}
 
-	return (*head);
+	new_node->str = strdup(str);
+	new_node->len = str_len;
+
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
 }
