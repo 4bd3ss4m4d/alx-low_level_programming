ff6a3d6f506102c3c9828a70b0e219a4616b767a
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-06-23 18:31:00 +0100

    Changing function syntax and description for 3. Add node at the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,44 +1,43 @@
 #include "lists.h"
-/**
- * add_node_end - Add a new node at the end of a list.
- * @head: Address of the first node of a list.
- * @str: Address of the string to insert into the new node.
- *
- * Return: Address of the new node.
- **/
+#include <string.h>
+#include <stdlib.h>
 
+/**
+ * add_node_end - Adds a new node at the end of a list_t list.
+ * @head: Pointer to a pointer to the head of the linked list.
+ * @str: String to be duplicated and added as the data of the new node.
+ *
+ * Return: The address of the new element, or NULL if it failed.
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *temp, *temp2;
-	unsigned int length = 0;
+	list_t *new_node, *current = *head;
+	size_t str_len = 0;
 
-	if (str == NULL)
-		return (NULL);
-
-	temp = malloc(sizeof(list_t));
-	if (temp == NULL)
-		return (NULL);
-
-	temp->str = strdup(str);
-	if (temp->str == NULL)
+	new_node = (list_t *)malloc(sizeof(list_t));
+	if (new_node == NULL)
 	{
-		free(temp);
 		return (NULL);
 	}
-	while (str[length])
-		length++;
-	temp->len = length;
-	temp->next = NULL;
+
+	while (str[str_len] != '\0')
+	{
+		str_len++;
+	}
+
+	new_node->str = strdup(str);
+	new_node->len = str_len;
 
 	if (*head == NULL)
 	{
-		*head = temp;
-		return (temp);
+		*head = new_node;
+		return (new_node);
 	}
+	while (current->next != NULL)
+	{
+		current = current->next;
+	}
+	current->next = new_node;
 
-	temp2 = *head;
-	while (temp2->next)
-		temp2 = temp2->next;
-	temp2->next = temp;
-	return (temp);
+	return (new_node);
 }
