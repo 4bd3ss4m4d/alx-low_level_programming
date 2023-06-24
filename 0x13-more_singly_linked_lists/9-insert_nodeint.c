168b45563b300025686cff5c6e9c94144fab7942
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-06-24 18:43:34 +0100

    Changing function syntax and description for 9. Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,45 +1,53 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * insert_nodeint_at_index - inserts a new node in a linked list,
- * at a given position
- * @head: pointer to the first node in the list
- * @idx: index where the new node is added
- * @n: data to insert in the new node
+ * insert_nodeint_at_index - Inserts a new node at a given position in a linked
+ *			     list.
+ * @head: Pointer to a pointer to the head of the linked list.
+ * @idx: The index at which the new node should be inserted.
+ * @n: The value to be stored in the new node.
  *
- * Return: pointer to the new node, or NULL
+ * Return: Address of the new node on success, or NULL if it failed.
+ *	   If it's not possible to add the new node at index idx, do not add it
+ *	   and return NULL.
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i;
-	listint_t *new;
-	listint_t *temp = *head;
+	listint_t *new_node, *current = *head, *previous = NULL;
+	unsigned int count = 0;
 
-	new = malloc(sizeof(listint_t));
-	if (!new || !head)
+	new_node = (listint_t *)malloc(sizeof(listint_t));
+	if (new_node == NULL)
+	{
 		return (NULL);
-
-	new->n = n;
-	new->next = NULL;
-
-	if (idx == 0)
-	{
-		new->next = *head;
-		*head = new;
-		return (new);
 	}
 
-	for (i = 0; temp && i < idx; i++)
+	new_node->n = n;
+
+	while (current != NULL && count < idx)
 	{
-		if (i == idx - 1)
-		{
-			new->next = temp->next;
-			temp->next = new;
-			return (new);
-		}
-		else
-			temp = temp->next;
+		previous = current;
+		current = current->next;
+		count++;
 	}
 
-	return (NULL);
+	if (count != idx)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = current;
+	if (previous == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		previous->next = new_node;
+	}
+
+	return (new_node);
 }
