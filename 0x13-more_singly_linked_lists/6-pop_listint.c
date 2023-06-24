360f5eced99817792f638f34fabb812caad64a99
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-06-24 13:29:20 +0100

    Changing function syntax and description for 6. Pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,24 +1,26 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * pop_listint - deletes the head node of a linked list
- * @head: pointer to the first element in the linked list
+ * pop_listint - Deletes the head node of a listint_t linked list.
+ * @head: Pointer to the pointer to the head of the list.
  *
- * Return: the data inside the elements that was deleted,
- * or 0 if the list is empty
+ * Return: The data (n) of the deleted head node, or 0 if the list is empty.
  */
 int pop_listint(listint_t **head)
 {
-	listint_t *temp;
-	int num;
+	listint_t *temp = *head;
+	int deleted_data;
 
-	if (!head || !*head)
+	if (*head == NULL)
+	{
 		return (0);
+	}
 
-	num = (*head)->n;
-	temp = (*head)->next;
-	free(*head);
-	*head = temp;
+	*head = (*head)->next;
+	deleted_data = temp->n;
+	free(temp);
 
-	return (num);
+	return (deleted_data);
 }
