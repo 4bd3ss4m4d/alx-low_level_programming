9b3883b5fbff80d0476502cd4c549ee1210d19cf
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-06-24 11:00:53 +0100

    Changing function syntax and description for 2. Add node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,23 +1,29 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * add_nodeint - adds a new node at the beginning of a linked list
- * @head: pointer to the first node in the list
- * @n: data to insert in that new node
+ * add_nodeint - Adds a new node at the beginning of a listint_t list.
+ * @head: Pointer to the pointer to the head of the list.
+ * @n: Integer value to be stored in the new node.
  *
- * Return: pointer to the new node, or NULL if it fails
+ * Return: The address of the new element on success, or NULL on failure.
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new;
+	listint_t *new_node;
 
-	new = malloc(sizeof(listint_t));
-	if (!new)
+	new_node = (listint_t *)malloc(sizeof(listint_t));
+	if (new_node == NULL)
+	{
 		return (NULL);
+	}
 
-	new->n = n;
-	new->next = *head;
-	*head = new;
+	new_node->n = n;
+	new_node->next = *head;
 
-	return (new);
+
+	*head = new_node;
+
+	return (new_node);
 }
