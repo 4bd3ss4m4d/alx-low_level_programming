d48559063f048f57dabc542a3dc189d6656f34c5
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-07-19 11:00:59 +0100

    3. >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,52 +1,52 @@
 #include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * hash_table_set - Insert a key/value pair into a hash table.
- * @ht: The hash table to insert into.
- * @key: The key of the pair to insert.
- * @value: The value of the pair to insert.
+ * hash_table_set - Adds a key/value pair to the hash table.
+ * @ht: The hash table to add/update the key/value pair.
+ * @key: The key to be added to the hash table. It cannot be an empty string.
+ * @value: The value associated with the key. It will be duplicated.
+ *	 It can be an empty string.
  *
- * Return: 1 on success, 0 on failure.
+ * Return: 1 if the operation is successful, 0 otherwise.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	hash_node_t *new_node;
 	unsigned long int index, i;
-	hash_node_t *new;
-	char *value_cp;
 
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+	if (ht == NULL || key == NULL || *key == '\0')
+	{
 		return (0);
-
-	value_cp = strdup(value);
-	if (value_cp == NULL)
-		return (0);
+	}
 
 	index = key_index((const unsigned char *)key, ht->size);
-	for (i = index; ht->array[i]; i++)
+
+	/* Check if key already exists in the Hash table */
+	for (i = index; ht->array[i] != NULL; i++)
 	{
-		if (strcmp(ht->array[i]->key, key) == 0)
+		if (strcmp(key, ht->array[i]->key) == 0)
 		{
+			/* Update the value associated with they key */
 			free(ht->array[i]->value);
-			ht->array[i]->value = value_cp;
+			ht->array[i]->value = strdup(value);
 			return (1);
 		}
 	}
 
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
+	/* If they key does not exist, create a new node */
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
 	{
-		free(value_cp);
 		return (0);
 	}
-	new->key = strdup(key);
-	if (new->key == NULL)
-	{
-		free(new);
-		return (0);
-	}
-	new->value = value_cp;
-	new->next = ht->array[index];
-	ht->array[index] = new;
+
+	/* Add node at the beginning of the linked list */
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 
 	return (1);
 }
