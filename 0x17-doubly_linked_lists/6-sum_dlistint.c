d6e1b7930399aa11459529f9dfa7f1e6afb938eb
Abdessamad HADDOUCHE <abdessamad.hadd@gmail.com>
2023-07-30 01:51:16 +0100

    Changing function description and syntax for 6. Sum list

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,28 +1,24 @@
 #include "lists.h"
 
 /**
- * sum_dlistint - returns the sum of all the data of a doubly linked list
- * @head: head of the list
+ * sum_dlistint - returns the sum of all the data (n) of a dlistint_t linked
+ *                list
+ * @head: pointer to the head of the list
  *
- * Return: sum of the data
+ * Return: the sum of all the data (n) of the linked list, or 0 if the list is
+ *         empty
  */
 int sum_dlistint(dlistint_t *head)
 {
-	int sum = 0;
+	dlistint_t *current = head;
+	int result = 0;
 
-	if (head != NULL)
+	while (current != NULL)
 	{
-		while (head->prev != NULL)
-		{
-			head = head->prev;
-		}
+		result += current->n;
 
-		while (head != NULL)
-		{
-			sum += head->n;
-			head = head->next;
-		}
+		current = current->next;
 	}
 
-	return (sum);
+	return (result);
 }
